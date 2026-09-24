82ffa1ddb38220c0592de1e93374b557e75aba05
RRowDY <durellwalzer@gmail.com>
2026-09-24 01:45:56 -0700

    add shared site config for nav & metadata

---
+++ content/site.ts
@@ -0,0 +1,18 @@
+export const site = {
+  name: "Joshua",
+  description: "I'm a software engineer with a passion for building web applications that are fast, responsive, and easy to use.",
+  nav: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+],
+} as const;
